f85fb13b51320479b1f69789f5b399539385a9d0
Brisa Ortiz <briithomps@gmail.com>
2023-02-27 23:54:17 -0300

    este es nuestro primer commit

---
+++ formato2.js
@@ -0,0 +1,14 @@
+document.getElementById('boton').onclick = function () {
+    console.log("mi informacion");
+    document.getElementById('mi_info').innerHTML = "Fecha de nacimiento: 25-05-02";
+    document.getElementById('dato2').innerHTML = "Documento: 44106568";
+    document.getElementById('dato3').innerHTML = "Contacto: 11601927919";
+    document.getElementById('dato4').innerHTML = "Localidad: Ezeiza";
+    document.getElementById('dato5').innerHTML = "Nacionalidad: Argentina";
+}
+
+document.getElementById('boton2').onclick = function () {
+    console.log("ver");
+    document.getElementById('dato6').innerHTML = "Desarrollo Front-End (01/2023)";
+    document.getElementById('dato7').innerHTML = "Desarrollo Back-End (10/2022)";
+}  
