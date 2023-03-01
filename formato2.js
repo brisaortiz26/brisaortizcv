a0fdf49683ceba7d4ba81d4d6f3564e4aa0b5dca
brisaortiz26 <briithomps@gmail.com>
2023-02-28 23:41:53 -0300

    commit 1

--- formato2.js
+++ formato2.js
@@ -5,10 +5,12 @@ document.getElementById('boton').onclick = function () {
     document.getElementById('dato3').innerHTML = "Contacto: 11601927919";
     document.getElementById('dato4').innerHTML = "Localidad: Ezeiza";
     document.getElementById('dato5').innerHTML = "Nacionalidad: Argentina";
+    document.getElementById('dato9').innerHTML = "Email: briithomps@gmail.com";
 }
 
 document.getElementById('boton2').onclick = function () {
     console.log("ver");
     document.getElementById('dato6').innerHTML = "Desarrollo Front-End (01/2023)";
     document.getElementById('dato7').innerHTML = "Desarrollo Back-End (10/2022)";
-}  
+    document.getElementById('dato8').innerHTML = "Lenguaje Java";
+}
